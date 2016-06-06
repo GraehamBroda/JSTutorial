bdd38ef588e7a22c1e7f0891eae2fd3bc1cd3eef
Graeham Broda <graeham.broda@td.com>
2016-06-06 16:22:40 -0400

    final commit from work

--- src/app/waitList/waitList.controller.js
+++ src/app/waitList/waitList.controller.js
@@ -11,6 +11,7 @@
         var vm = this;
         
         var fireParties = new Firebase('https://waitandeatgb.firebaseio.com/parties');
+        var fireTextMessages = new Firebase('https://waitandeatgb.firebaseio.com/textMessages');
         
         function Party() {
             this.name = '';
@@ -20,18 +21,34 @@
             this.notified=false;
         }
         
-        vm.newParty = new Party()
-        vm.parties = $firebaseArray(fireParties)  
+        vm.newParty = new Party();
+        vm.parties = $firebaseArray(fireParties) ; 
         vm.addParty = addParty;
-        vm.removeParty = removeParty
+        vm.removeParty = removeParty;
+        vm.sendTextMessage = sendTextMessage;
         
         function addParty() {
             vm.parties.$add(vm.newParty);
+            vm.newParty = new Party();
         }
         
         function removeParty(party){
             vm.parties.$remove(party);
         }
+        
+        function sendTextMessage(party) {
+            var newTextMessage = {
+                name: party.name,
+                phoneNumber: party.size,
+                size: party.size
+            }
+            fireTextMessages.push(newTextMessage);
+            party.notified = true;
+            vm.parties.$save(party);
+        }
+
+    
+        
     }
     
 })();
