5d3c2c18e4458a8cc5a80b50536a432546a34461
Graeham Broda <graeham.broda@td.com>
2016-06-06 13:41:46 -0400

    added firebase database and some functionality

--- src/app/waitList/waitList.controller.js
+++ src/app/waitList/waitList.controller.js
@@ -5,10 +5,28 @@
         .module('app.waitList')
         .controller('WaitListController', WaitListController);
     
-    function WaitListController() {
+    WaitListController.$inject = ['$firebaseArray'];
+    
+    function WaitListController($firebaseArray) {
         var vm = this;
         
-        vm.parties = [1,2,3,4];
+        var fireParties = new Firebase('https://waitandeatgb.firebaseio.com/parties');
+        
+        function Party() {
+            this.name = '';
+            this.phone='';
+            this.size='';
+            this.done=false;
+            this.notified=false;
+        }
+        
+        vm.newParty = new Party()
+        vm.parties = $firebaseArray(fireParties)  
+        vm.addParty = addParty;
+        
+        function addParty() {
+            vm.parties.$add(vm.newParty);
+        }
     }
     
 })();
